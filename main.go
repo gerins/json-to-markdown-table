583aee2c370e2faf61f00a4a4b90c6b342abf06a
Garin <gerin.rin@gmail.com>
2024-12-28 23:33:19 +0700

    add readme

--- main.go
+++ main.go
@@ -138,43 +138,15 @@ func main() {
 	// Example JSON input
 	inputJSON := `
 	{
-		"quiz": {
-			"sport": {
-				"q1": {
-					"question": "Which one is correct team name in NBA?",
-					"options": [
-						"New York Bulls",
-						"Los Angeles Kings",
-						"Golden State Warriros",
-						"Huston Rocket"
-					],
-					"answer": "Huston Rocket"
-				}
-			},
-			"maths": {
-				"q1": {
-					"question": "5 + 7 = ?",
-					"options": [
-						"10",
-						"11",
-						"12",
-						"13"
-					],
-					"answer": "12"
-				},
-				"q2": {
-					"question": "12 - 8 = ?",
-					"options": [
-						"1",
-						"2",
-						"3",
-						"4"
-					],
-					"answer": "4"
-				}
-			}
+		"user": {
+		  "id": 1,
+		  "name": "John Doe",
+		  "email": "john@example.com",
+		  "is_active": true,
+		  "roles": ["admin", "user"]
 		}
-	}`
+	  }
+	`
 
 	// Generate markdown
 	markdown, err := GenerateMarkdown(inputJSON)
